0d804b8b8ea7f1c2aeb79d60b6b880bfda999ca1
HereisLuvika <kth882500@gmail.com>
2025-01-20 09:45:43 +0900

    변수명 역할 중심으로 수정

--- Assets/Scripts/ShipMove.cs
+++ Assets/Scripts/ShipMove.cs
@@ -5,30 +5,30 @@ public class ShipMove : MonoBehaviour
 {
     public Tilemap tilemap;
     public float moveSpeed = 5f; // 이동 속도는 함선마다 조정 필요함, 테스트 빌드 속도는 5f
-    public Transform selectedShip;
+    public Transform movingShip;
     private Vector3 targetPosition;
     // Update is called once per frame
     void Update()
     {
         // AssignShip이 전에는 작동 안 함
-        if (selectedShip == null)
+        if (movingShip == null)
         {
             return;
         }
         // 타겟 위치 변경이 확인될 경우 이동
-        if(selectedShip.position != targetPosition)
+        if(movingShip.position != targetPosition)
         {
-            Vector3 moveDir = (targetPosition - selectedShip.position).normalized; // 방향 벡터
+            Vector3 moveDir = (targetPosition - movingShip.position).normalized; // 방향 벡터
             float moveAngle = Mathf.Atan2(moveDir.y, moveDir.x) * Mathf.Rad2Deg; // Mathf.Atan2는 y와 x로 각도를 계산하여 라디안 각도 반환, Mathf.Rad2Deg로 라디안을 Degree로 변환
-            selectedShip.rotation = Quaternion.Euler(0, 0, moveAngle); //Quaternion.Euler(0, 0, moveAngle)는 각도 기반으로 회전 생성
-            selectedShip.position = Vector3.MoveTowards(selectedShip.position, targetPosition, moveSpeed * Time.deltaTime); // MoveTowards로 선형 이동
+            movingShip.rotation = Quaternion.Euler(0, 0, moveAngle); //Quaternion.Euler(0, 0, moveAngle)는 각도 기반으로 회전 생성
+            movingShip.position = Vector3.MoveTowards(movingShip.position, targetPosition, moveSpeed * Time.deltaTime); // MoveTowards로 선형 이동
         }
     }  
     // 함선 할당 함수, ShipBehavior에서 선택한 함선이 selectedShip이 되어 이동 함선, 현재 위치를 할당
     public void AssignShip(Transform selectedShip)
     {
-        this.selectedShip = selectedShip;
-        targetPosition = this.selectedShip.position;
+        movingShip = selectedShip;
+        targetPosition = movingShip.position;
     }
     public void InitialMove() // 타겟 위치 변경 함수
     {
