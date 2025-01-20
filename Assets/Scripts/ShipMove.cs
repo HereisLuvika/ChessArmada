19b7aeb4a5688ce8079a1c8335ca6e3d57b40f8c
HereisLuvika <kth882500@gmail.com>
2025-01-20 09:41:07 +0900

    ship->selectedShip으로 변수명 변경

--- Assets/Scripts/ShipMove.cs
+++ Assets/Scripts/ShipMove.cs
@@ -5,30 +5,30 @@ public class ShipMove : MonoBehaviour
 {
     public Tilemap tilemap;
     public float moveSpeed = 5f; // 이동 속도는 함선마다 조정 필요함, 테스트 빌드 속도는 5f
-    public Transform ship;
+    public Transform selectedShip;
     private Vector3 targetPosition;
     // Update is called once per frame
     void Update()
     {
         // AssignShip이 전에는 작동 안 함
-        if (ship == null)
+        if (selectedShip == null)
         {
             return;
         }
         // 타겟 위치 변경이 확인될 경우 이동
-        if(ship.position != targetPosition)
+        if(selectedShip.position != targetPosition)
         {
-            Vector3 moveDir = (targetPosition - ship.position).normalized; // 방향 벡터
+            Vector3 moveDir = (targetPosition - selectedShip.position).normalized; // 방향 벡터
             float moveAngle = Mathf.Atan2(moveDir.y, moveDir.x) * Mathf.Rad2Deg; // Mathf.Atan2는 y와 x로 각도를 계산하여 라디안 각도 반환, Mathf.Rad2Deg로 라디안을 Degree로 변환
-            ship.rotation = Quaternion.Euler(0, 0, moveAngle); //Quaternion.Euler(0, 0, moveAngle)는 각도 기반으로 회전 생성
-            ship.position = Vector3.MoveTowards(ship.position, targetPosition, moveSpeed * Time.deltaTime); // MoveTowards로 선형 이동
+            selectedShip.rotation = Quaternion.Euler(0, 0, moveAngle); //Quaternion.Euler(0, 0, moveAngle)는 각도 기반으로 회전 생성
+            selectedShip.position = Vector3.MoveTowards(selectedShip.position, targetPosition, moveSpeed * Time.deltaTime); // MoveTowards로 선형 이동
         }
     }  
     // 함선 할당 함수, ShipBehavior에서 선택한 함선이 selectedShip이 되어 이동 함선, 현재 위치를 할당
     public void AssignShip(Transform selectedShip)
     {
-        ship = selectedShip;
-        targetPosition = ship.position;
+        this.selectedShip = selectedShip;
+        targetPosition = this.selectedShip.position;
     }
     public void InitialMove() // 타겟 위치 변경 함수
     {
